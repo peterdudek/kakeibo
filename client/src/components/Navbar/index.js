e2c4f1d8d8e07b9d49e43f021b573fe1331ed0e2
Piotr Dudek <piotr72@gmail.com>
2021-01-18 00:38:34 -0500

    scrollable tables, new buttons, new navbar layout, footer, logo

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.css";
+import logoEl from "../../img/IMG_1844.PNG";
 
 // import user API and add logout function
 import userAPI from "../../utils/userAPI";
@@ -14,31 +15,51 @@ function logout() {
 function Navbar(props) {
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-white borderline">
+      <div>
+        <img alt="kakeibo-symbol" src={logoEl} height="50px" />
+      </div>
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">Welcome {props.username}</a>
+        <a className="navbar-brand" href="/">Hi {props.username}</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
+        <div className="inner">
+          <h1 className='kakeiboFont'>Kakeibo 家計簿</h1>
+          {/* <h1 className='mt-0 ml-0 mb-3 text-dark p-4 pt-3 pl-3 kakeiboFont'>Kakeibo 家計簿</h1> */}
+          </div>
+        <div className="collapse navbar-collapse d-flex" id="navbarNav">
+          <ul className="navbar-nav ml-auto p-2 col-example">
+            <li className="nav-item borders">
               <a className="nav-link active" aria-current="page" href="/">Home</a>
             </li>
 
             {/* added Logout button */}
             {props.username ?
-              <li className="nav-item">
-                <a className="nav-link active testButton" aria-current="page"
-                  href="/"
-                  onClick={() => logout()}
-                >Logout</a>
-              </li>
-              : <> </>
-            }
+            <li className="nav-item borders">
+              <a className="nav-link active testButton" aria-current="page"
+                href="/"
+                onClick={() => logout()}
+              >Logout</a>
+            </li>
+            : <> </>
+          }
           </ul>
         </div>
       </div>
+      {/* <div>
+        <ul className="navbar-nav">
+          {props.username ?
+            <li className="nav-item borders">
+              <a className="nav-link active testButton" aria-current="page"
+                href="/"
+                onClick={() => logout()}
+              >Logout</a>
+            </li>
+            : <> </>
+          }
+        </ul>
+      </div> */}
     </nav>
   )
 }
--- client/src/components/Table/index.js
+++ client/src/components/Table/index.js
@@ -3,11 +3,18 @@ import "./style.css";
 
 export function Table({ children }) {
 	return (
-		<table className=" table table-dark table-striped text-center text-break tableRound">
-			<tbody>
+		<div 
+		className="scrollable"
+		>
+		<table className=
+		"table table-dark table-striped border-top-0 text-center text-break tableRound"
+		// "table"
+		>
+			<tbody className="border-top-0">
 				{children}
 			</tbody>
 		</table>
+		</div>
 	);
 }
 
@@ -16,7 +23,7 @@ export function Tr(props) {
 }
 
 export function Td({ children }) {
-	return <td >{children}</td>;
+	return <td className="border-top-0">{children}</td>;
 }
 
 export default Table;
